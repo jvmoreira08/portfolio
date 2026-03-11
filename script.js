714bf9437d0c25d6ffce5ae7a54503aa1d0e5ea3
João Victor ~ dev <jvmoreira08@gmail.com>
2026-03-10 22:31:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ document.body.classList.toggle("dark")
 
 }
 
+// validação formulário
+
+document
+.getElementById("formContato")
+.addEventListener("submit", function(event){
+  
+event.preventDefault()
+
+let nome = document.getElementById("nome").value
+let email = document.getElementById("email").value
+let mensagem = document.getElementById("mensagem").value
+
+if(nome === "" || email === "" || mensagem === "")"{
+alert("Preencha todos os campos")
+return 
+}
+
+let emailValido = /\S+@\S+\.\S+/
+
+if(!emailValido.test(email)){
+alert("Email inválido")
+return
+}
+                  
+alert("Mensagem enviada com sucesso!")
+
+document.getElementById("formContato").reset()
+
+})
+
